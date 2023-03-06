8d7fa7f0b8b62e4456b3ed8a9f26bb13d0d133b5
Luis Cordova <luis.cordova.garrido@gmail.com>
2023-03-06 19:12:04 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+import Formulario from "./components/Formulario";
+import { Header } from "./components/Header";
+import ListadoPacientes from "./components/ListadoPacientes";
+
+
+function App() {
+
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
+
+  const eliminarPaciente = ( id ) => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id)
+    setPacientes(pacientesActualizados)
+  }
+
+  useEffect(() => {
+    // const obtenerLS = () => {
+    //   const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+    //   setPacientes(pacientesLS);
+    // }
+    // obtenerLS();
+    const pacientesLocal = JSON.parse(localStorage.getItem('pacientes'));
+    pacientesLocal?.length > 0 && setPacientes(pacientesLocal);
+  }, [])
+
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ))
+  }, [pacientes])
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes = {pacientes}
+          setPacientes = {setPacientes}
+          paciente = {paciente}
+          setPaciente = {setPaciente}
+        />
+        <ListadoPacientes
+          pacientes= {pacientes}
+          setPaciente= {setPaciente}
+          eliminarPaciente = {eliminarPaciente}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
